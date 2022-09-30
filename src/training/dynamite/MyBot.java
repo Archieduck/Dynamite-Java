a2e45233d6f00c5c4f97b3e5b7ba7a1c6451ec30
aduck1 <archie.duck@aexp.com>
2022-09-30 14:33:50 +0100

    bot added

--- src/training/dynamite/MyBot.java
+++ src/training/dynamite/MyBot.java
@@ -3,7 +3,19 @@ package training.dynamite;
 import com.softwire.dynamite.bot.Bot;
 import com.softwire.dynamite.game.*;
 
+import java.util.List;
+import java.util.Random;
+
 public class MyBot implements Bot {
+
+    private int dynamiteCount = 99;
+    private int theirDynamiteCount = 100;
+    private int dynamiteChance;
+    private int theirDynamiteChance = 1;
+    private List<Move> lastTen;
+    private boolean sameAfterDraw;
+    private Move lastDrawOutcome;
+    private int movesSinceOppDynamite;
 
     public MyBot() {
         // Are you debugging?
@@ -13,9 +25,114 @@ public class MyBot implements Bot {
 
     @Override
     public Move makeMove(Gamestate gamestate) {
-        // Are you debugging?
-        // Put a breakpoint in this method to see when we make a move
-        return Move.R;
+        dynamiteChance += 1;
+        if (gamestate.getRounds().size() > 0) {  // keep count of their dynamite
+            Round lastRound = (Round) gamestate.getRounds().get(gamestate.getRounds().size() - 1);
+            if (lastRound.getP2() == Move.D) {
+                movesSinceOppDynamite = 0;
+                theirDynamiteCount -= 1;
+            } else {
+                movesSinceOppDynamite += 1;
+            }
+        }
+        if (gamestate.getRounds().size() > 1) {
+            Round lastRound = (Round) gamestate.getRounds().get(gamestate.getRounds().size() - 1);
+            Round lastRoundAgain = (Round) gamestate.getRounds().get(gamestate.getRounds().size() - 2);
+            if (lastRoundAgain.getP2() == lastRoundAgain.getP1()){ // get the move they played after the last draw
+                if (lastRound.getP2() == lastDrawOutcome) {
+                    sameAfterDraw = true;
+                } else {
+                    sameAfterDraw = false;
+                }
+                lastDrawOutcome = lastRound.getP2();
+            }
+
+            if (lastRound.getP2() == lastRoundAgain.getP2()){  //if they play the same move twice, beat it
+                return beatTheirLastMove(lastRound.getP2());
+            } else if (dynamiteCount > 0 && chanceOfDynamite(dynamiteCount, dynamiteChance, gamestate)){ //plays my dynamite randomly
+                dynamiteChance = 0;
+                dynamiteCount -= 1;
+                return Move.D;
+            } else if (lastRound.getP2().equals(lastRound.getP1())) { // if last round was a draw
+                if (lastDrawOutcome != null && gamestate.getRounds().size() > 0 && sameAfterDraw) {
+                    return beatTheirLastMove(lastDrawOutcome);
+                }else if (lastDrawOutcome != null && !sameAfterDraw){
+                    return playTheirLastMove(lastDrawOutcome);
+                }
+                dynamiteCount -= 1;
+                return Move.D;
+            }else {
+                return beatTheirLastMove(lastRound.getP2());
+            }
+
+        } else return Move.D;
+
+    }
+
+    //else if (chanceOfDynamite(theirDynamiteCount, movesSinceOppDynamite, gamestate)) {
+    //                    return Move.W;
+    //            }
+
+    public Move playTheirLastMove(Move theirLastMove) {
+        switch (theirLastMove) {
+            case R:
+                return Move.R;
+            case P:
+                return Move.P;
+            case S:
+                return Move.S;
+            case D:
+                return Move.D;
+            case W:
+                return Move.W;
+            default:
+                throw new RuntimeException("Invalid last move from P111");
+        }
+
+
+    }
+
+    public Move beatTheirLastMove(Move theirLastMove) {
+        switch (theirLastMove) {
+            case R:
+                return Move.P;
+            case P:
+                return Move.S;
+            case S:
+                return Move.R;
+            case D:
+                return Move.W;
+            case W:
+                return Move.R;
+            default:
+                throw new RuntimeException("Invalid last move from P2");
+        }
+
+
+    }
+
+    public Boolean chanceOfDynamite(int dynamiteCount, int dynamiteChance, Gamestate gamestate) {
+        if (gamestate.getRounds().size() > 0){
+            Round lastRound = (Round)gamestate.getRounds().get(gamestate.getRounds().size() - 1);
+            Move lastMove = lastRound.getP2();
+        }
+        Random rand = new Random();
+        int int_random = rand.nextInt(11);
+        if (dynamiteCount > 0) {
+            if (dynamiteChance >= int_random || dynamiteCount > (1000 - gamestate.getRounds().size())) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return false;
+    }
+
+    public Move getRandomMove() {
+        int randomNumberBetween0And3 = (int)Math.floor(Math.random() * 3.0);
+        Move[] possibleMoves = new Move[]{Move.R, Move.P, Move.S};
+        Move randomMove = possibleMoves[randomNumberBetween0And3];
+        return randomMove;
     }
 
 }
